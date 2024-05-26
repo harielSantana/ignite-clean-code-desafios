47b58e48e6399ad9cf4157f3de82a8d8492c03f0
Hariel Santana <hariol.santana138@gmail.com>
2024-05-26 01:01:58 -0300

    feat(01): changes on variables names.

--- desafios/01-nomenclatura-de-variaveis.ts
+++ desafios/01-nomenclatura-de-variaveis.ts
@@ -1,6 +1,6 @@
 // Nomenclatura de variáveis
 
-const list = [
+const categories = [
   {
     title: 'User',
     followers: 5
@@ -19,37 +19,37 @@ const list = [
   },
 ]
 
-export default async function getData(req, res) {
-  const github = String(req.query.username)
+export default async function findGithubUsernameAndGetCategory(req, res) {
+  const username = String(req.query.username)
 
-  if (!github) {
+  if (!username) {
     return res.status(400).json({
       message: `Please provide an username to search on the github API`
     })
   }
 
-  const response = await fetch(`https://api.github.com/users/${github}`);
+  const searchGithubUsernameResponse = await fetch(`https://api.github.com/users/${username}`);
 
-  if (response.status === 404) {
+  if (searchGithubUsernameResponse.status === 404) {
     return res.status(400).json({
-      message: `User with username "${github}" not found`
+      message: `User with username "${username}" not found`
     })
   }
 
-  const data = await response.json()
+  const findedUserData = await searchGithubUsernameResponse.json()
 
-  const orderList = list.sort((a, b) =>  b.followers - a.followers); 
+  const orderCategoriesByFollowers = categories.sort((a, b) =>  b.followers - a.followers);
 
-  const category = orderList.find(i => data.followers > i.followers)
+  const findCategoryWithMostFollowers = orderCategoriesByFollowers.find(i => findedUserData.followers > i.followers)
 
   const result = {
-    github,
-    category: category.title
+    username,
+    category: findCategoryWithMostFollowers.title
   }
 
   return result
 }
 
-getData({ query: {
+findGithubUsernameAndGetCategory({ query: {
   username: 'josepholiveira'
 }}, {})
